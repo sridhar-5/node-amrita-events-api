e65947835ffde93cad4cc5a2381383553e8f7e99
sridhar-5 <kv.sridharsai@gmail.com>
2021-08-21 16:15:45 +0530

    login register deployment

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ DBConnection();
 app.use("/api/login", LoginUser);
 app.use("api/register", RegisterUser);
 
+app.get("/", (request, response) => {
+  response.send("Welcome to the AmritaEvents Api HomePage");
+});
+
 const PORT = process.env.PORT || 3000;
 //listening
 app.listen(PORT, () => {
